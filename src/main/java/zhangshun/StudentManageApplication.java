a2700a9ea2f55c3ea7b0551ec750262e1d58b8e4
Zshxy1013 <838511659@qq.com>
2022-09-21 08:16:45 +0800

    Initial commit

---
+++ src/main/java/zhangshun/StudentManageApplication.java
@@ -0,0 +1,13 @@
+package zhangshun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zhangshun/StudentManageApplicationTests.java
@@ -0,0 +1,13 @@
+package zhangshun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
